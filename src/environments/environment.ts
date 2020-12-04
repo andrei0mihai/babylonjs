45798bcdf5bba1d9a9ebf3bcca82a15c2a9a562e
Andrei Mihai <andrei.mihai@softvision.com>
2020-12-04 16:41:51 +0200

    fix lint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyBWjMa8adLpGBRWh66bw8lkDTbHbz_8wUo",
-    authDomain: "babylon-js-with-angular.firebaseapp.com",
-    databaseURL: "https://babylon-js-with-angular.firebaseio.com",
-    projectId: "babylon-js-with-angular",
-    storageBucket: "babylon-js-with-angular.appspot.com",
-    messagingSenderId: "824212666012",
-    appId: "1:824212666012:web:d886e08515bb06f735df59"
-  }
+  firebaseConfig: {
+    apiKey: 'AIzaSyBWjMa8adLpGBRWh66bw8lkDTbHbz_8wUo',
+    authDomain: 'babylon-js-with-angular.firebaseapp.com',
+    databaseURL: 'https://babylon-js-with-angular.firebaseio.com',
+    projectId: 'babylon-js-with-angular',
+    storageBucket: 'babylon-js-with-angular.appspot.com',
+    messagingSenderId: '824212666012',
+    appId: '1:824212666012:web:d886e08515bb06f735df59',
+  },
 };
 
 /*
